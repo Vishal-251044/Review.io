ca6007b46faa15c88a729ec481da08c42fbea830
Vishal Subhash Chavan <157942851+Vishal-251044@users.noreply.github.com>
2025-07-30 15:11:56 +0530

    Update Footer.js

--- frontend/src/components/Footer.js
+++ frontend/src/components/Footer.js
@@ -4,9 +4,9 @@ import "./Footer.css";
 const Footer = () => {
     return (
         <footer className="footer">
-            <div className="footer-logo">Review.NLP</div>
-            <div className="footer-text">Review.NLP creates a platform for public review submissions. Utilizing NLP, it determines whether feedback is positive or negative and assesses the project's overall success based on the analyzed reviews.</div>
-            <div className="footer-rights">© 2025 Review.NLP. All rights reserved.</div>
+            <div className="footer-logo">Review.io</div>
+            <div className="footer-text">Review.io creates a platform for public review submissions. Utilizing NLP, it determines whether feedback is positive or negative and assesses the project's overall success based on the analyzed reviews.</div>
+            <div className="footer-rights">© 2025 Review.io. All rights reserved.</div>
         </footer>
     );
 };
